c637290bc52783f4fa531a4b863dc3d16fd83a54
Ciara R <100184461+creed725@users.noreply.github.com>
2023-10-22 15:51:56 -0700

    Modified the getForecast function

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "6f578b96aa9505bcce148ac22cb85794";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then((response) => {
+    displayForecast(response);
+
+    displayHourlyforecast(response.data.hourly); //Display hourly forecast
+  });
 }
 function displayTemperature(response) {
   console.log(response);
